8781ee9ffefe90b7f49167b419b243cbf9c32436
David Gordon <davidgordon12@live.ca>
2024-05-03 13:09:09 -0400

    Add newline support for the tty

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -19,7 +19,7 @@ static uint16_t* terminal_buffer;
 void terminal_initialize(void) {
 	terminal_row = 0;
 	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_color = vga_entry_color(VGA_COLOR_WHITE, VGA_COLOR_BLUE);
 	terminal_buffer = VGA_MEMORY;
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
@@ -34,8 +34,14 @@ void terminal_setcolor(uint8_t color) {
 }
 
 void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
-	const size_t index = y * VGA_WIDTH + x;
-	terminal_buffer[index] = vga_entry(c, color);
+	if(c == '\n') {
+		terminal_row++;
+		terminal_column = -1;
+	}
+	else {
+		const size_t index = y * VGA_WIDTH + x;
+		terminal_buffer[index] = vga_entry(c, color);
+	}
 }
 
 void terminal_putchar(char c) {
