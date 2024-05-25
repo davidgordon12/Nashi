f6f9e91f5f4a2a2d82e2d0f40ae9196036d4fba9
David Gordon <davidgordon12@live.ca>
2024-05-25 16:53:35 -0400

    Update run script

--- run.sh
+++ run.sh
@@ -3,13 +3,14 @@
 mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
+
+make kernel
  
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/nashi.bin isodir/boot/myos.bin
 cat > isodir/boot/grub/grub.cfg << EOF
 menuentry "nashi" {
 	multiboot /boot/nashi.bin
 }
 EOF
 
-make kernel
 make boot
